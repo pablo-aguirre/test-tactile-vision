ba923985e5fa29630af8607e719d5499880d2fa4
Pablo Aguirre <pablofabricio.aguirreguaman@studenti.unimi.it>
2024-11-11 15:44:24 +0100

    Initial Commit

---
+++ AR Sync Test/AR_Sync_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  AR_Sync_TestApp.swift
+//  AR Sync Test
+//
+//  Created by Pablo Aguirre on 11/11/24.
+//
+
+import SwiftUI
+
+@main
+struct AR_Sync_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
